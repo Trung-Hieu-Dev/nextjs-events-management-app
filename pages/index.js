fc2b86bc0726efac6e30860ffed17940b0d647ec
Trung-Hieu-Dev <dangtrunghieusg@gmail.com>
2023-06-22 09:13:41 +0700

    Rendering Events List in Home Page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
+import { getFeaturedEvents } from "../dummy-data";
+import EventList from "../pages/events";
+
 function HomePage() {
+  const featuredEvents = getFeaturedEvents();
   return (
     <div>
-      <h1>The Home Page</h1>
+      <div>
+        <EventList items={featuredEvents} />
+      </div>
     </div>
   );
 }
